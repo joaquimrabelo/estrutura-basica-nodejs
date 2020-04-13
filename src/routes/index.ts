93c409262be5a3bbe65b0df140c77f3db9ca0726
Joaquim Rabelo <joaquimrabello@gmail.com>
2020-04-13 11:00:41 -0300

    config prettier

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/', (request, response) => response.json({ Hello: 'World!' }));
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
+export default routes;
